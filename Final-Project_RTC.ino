64991e9af7c2ef3b3ec2a2f265f1b24900849cbb
SamMouradian <smouradian@nevada.unr.edu>
2024-04-22 11:34:51 -0700

    Samuel Mouradian - Initial Commit
    Real Time Clock implementation for final project.

---
+++ Final-Project_RTC.ino
@@ -0,0 +1,57 @@
+-//Final Project - Computer Engineering 301
+//RTC Implementation
+//Written by: Samuel Mouradian
+
+#include <RTClib.h>
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop(){
+  RTCtime();
+}
+
+void RTCtime(){
+  DateTime now = rtc.now();
+  int year = now.year();
+  int month = now.month();
+  int day = now.day();
+  int hour = now.hour();
+  int minute = now.minute();
+  int second = now.second();
+  char time[24] = {
+    'a',
+    't',
+    ' ',
+    hour / 10 + '0',
+    hour % 10 + '0',
+    ':',
+    minute / 10 + '0',
+    minute % 10 + '0',
+    ':',
+    second / 10 + '0',
+    second % 10 + '0',
+    'o',
+    'n',
+    month / 10 + '0',
+    month % 10 + '0',
+    '/',
+    day / 10 + '0',
+    day % 10 + '0',
+    '/',
+    (year / 1000) + '0',
+    (year % 1000 / 100) + '0',
+    (year % 100 / 10) + '0',
+    (year % 10) + '0',
+  };
+  for (int i = 0; i < 23; i++)
+  {
+    U0putchar(time[i]);
+  }
+}
+
+void U0putchar(unsigned char U0pdata){
+  while((*myUCSR0A & TBE)==0);
+  *myUDR0 = U0pdata;
+}
