37b9738302056e7e73a0cfcfc797553af779ec81
SamMouradian <smouradian@nevada.unr.edu>
2024-05-05 14:02:44 -0700

    Samuel Mouradian - RTC Finalized
    Changed RTC code so that the serial library isn't used.

--- realTimeClock/Final-Project_RTC.ino
+++ realTimeClock/Final-Project_RTC.ino
@@ -1,15 +1,38 @@
-//Final Project - Computer Engineering 301
-//RTC Implementation
-//Written by: Samuel Mouradian
+//Final Project:     Computer Engineering 301
+//File Description:  RTC Implementation
+//Written by:        Samuel Mouradian
 
 #include <RTClib.h>
+#define TBE 0x20
+RTC_DS1307 rtc;
+
+volatile unsigned char *myUCSR0A = (unsigned char *) 0x00C0;
+volatile unsigned char *myUCSR0B = (unsigned char *) 0x00C1;
+volatile unsigned char *myUCSR0C = (unsigned char *) 0x00C2;
+volatile unsigned int  *myUBRR0  = (unsigned int *)  0x00C4;
+volatile unsigned char *myUDR0   = (unsigned char *) 0x00C6;
+
 
 void setup() {
   Serial.begin(9600);
+  
+  if(!rtc.begin()){
+    U0putstring("Couldn't find RTC");
+    while(1);
+  }
+  else{
+    U0putstring("RTC Connected ");
+  }
+  if(!rtc.isrunning()){
+    U0putstring("RTC is not running, setting time.");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
 }
 
 void loop(){
   RTCtime();
+  U0putstring('\n');
+  delay(10000);
 }
 
 void RTCtime(){
@@ -20,7 +43,7 @@ void RTCtime(){
   int hour = now.hour();
   int minute = now.minute();
   int second = now.second();
-  char time[24] = {
+  char time[26] = {
     'a',
     't',
     ' ',
@@ -32,8 +55,10 @@ void RTCtime(){
     ':',
     second / 10 + '0',
     second % 10 + '0',
+    ' ',
     'o',
     'n',
+    ' ',
     month / 10 + '0',
     month % 10 + '0',
     '/',
@@ -54,4 +79,15 @@ void RTCtime(){
 void U0putchar(unsigned char U0pdata){
   while((*myUCSR0A & TBE)==0);
   *myUDR0 = U0pdata;
+}
+
+void U0putstring(char* U0pstring) {
+  while(*U0pstring) {
+      while(!(*myUCSR0A & TBE));
+      *myUDR0 = *U0pstring;
+      U0pstring++;
+  }
+  // Print a new line after printing the string
+  while(!(*myUCSR0A & TBE));
+  *myUDR0 = '\n';
 }
